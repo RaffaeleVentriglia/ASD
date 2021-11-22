0bb41da255e036099a64dad8d900d429aa707a68
raffaele <raffaeleventriglia6@gmail.com>
2021-11-23 00:42:17 +0100

    First Update

---
+++ Algoritmi/InsertionSort/main.cpp
@@ -0,0 +1,9 @@
+#include "main.hpp"
+
+int main() {
+    InsertionSort insertion;
+    int arr[9] = {1,3,5,7,9,2,4,6,8};
+    int key;
+    int length = 9;
+    insertion.algoritmo(arr, key, length);
+}
