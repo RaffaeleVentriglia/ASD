7bccf1d0093ac890482f38f1c28642cb4694a47a
Raffaele Ventriglia <raffaeleventriglia6@gmail.com>
2022-10-26 23:48:50 +0200

    Min K Heap

--- Algoritmi/InsertionSort/main.cpp
+++ Algoritmi/InsertionSort/main.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     Algoritmo InsertionSort;
-    int arr[9] = {1,3,5,7,9,2,4,6,8};
+    int arr[] = {1,3,5,7,9,2,4,6,8};
     int key;
-    int length = 9;
+    int length = 8;
     InsertionSort.InsertionSort(arr, key, length);
     return 0;
 }
