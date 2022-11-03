d856dee93b777cf177abb69ab29231106c80e02b
RaffaeleVentriglia <raffaeleventriglia6@gmail.com>
2022-11-03 21:34:59 +0100

    minor update

--- Algoritmi/main.cpp
+++ Algoritmi/main.cpp
@@ -10,13 +10,14 @@ using namespace std;
 
 int main() {
 
-    // inserire elementi di un array in un vector
+    // * inserire elementi di un array in un vector
+    // ! fai attenzione a come lo usi
+    // ? può essere utile sempre?
+    // TODO: inserire tutte le strutture dati
     int arr[] = {2,7,4,9,1,6};
     vector<int> vec(arr, arr + sizeof(arr)/sizeof(arr[0]));
     for(int i = 0; i < vec.size(); i++)
         cout << vec.at(i) << " ";
     cout << endl;
-
-    
     return 0;
 }
